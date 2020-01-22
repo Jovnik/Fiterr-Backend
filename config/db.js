30f2793b665ebe12c232f63ebe2467c8da3e9273
Matthew Dwyer <Matthew_Dwyer@Matthews-MacBook-Air.local>
2020-01-22 17:35:42 +1100

    fixed conflicts

--- config/db.js
+++ config/db.js
@@ -1,18 +1,8 @@
 const mongoose = require('mongoose');
-<<<<<<< HEAD
-const config = require('config');
-// const db = config.get('mongoURI');
-const db = `mongodb://localhost:27017/fiterr-test-db`;
-=======
->>>>>>> master
 
 const connectDB = async () => {
     try {
-<<<<<<< HEAD
-        await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
-=======
-        await mongoose.connect(process.env.DB_URL, { useNewUrlParser: true,  useUnifiedTopology: true });
->>>>>>> master
+        await mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log('MongoDB connected successfully ✔️✔️');
     } catch (err) {
         console.error(err.message);
